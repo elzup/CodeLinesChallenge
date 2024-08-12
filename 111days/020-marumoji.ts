6acb4a78fb31535f180ac46fbfb7527cc9585d10
elzup <guild0105@gmail.com>
2024-08-13 01:06:30 +0900

    add day020

---
+++ 111days/020-marumoji.ts
@@ -0,0 +1,15 @@
+// 1 -> ①
+
+// ① の charcode
+
+const charCode = '①'.charCodeAt(0)
+
+const marumoji = (n: number) => {
+  if (n < 1 || n > 20) return ''
+  return String.fromCharCode(charCode + n - 1)
+}
+
+console.log('1', marumoji(1))
+console.log('2', marumoji(2))
+console.log('19', marumoji(19))
+console.log('20', marumoji(20))
