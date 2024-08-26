ea87757f430f7bb83c20001b50ab202a0fbe83e4
elzup <guild0105@gmail.com>
2024-08-26 21:38:06 +0900

    update familia

--- 111days/00x-familia.js
+++ 111days/00x-familia.js
@@ -5,7 +5,9 @@
 
 const N = 4
 const SUM = 33
+const max = SUM - N + 1
 let p = 1
+const pad2 = (n) => String(n).padStart(2, ' ')
 
 /**
  * @param {number[]} m
@@ -19,7 +21,7 @@ let p = 1
  */
 
 const magic = (m, d, t) => {
-  let nextMax = SUM
+  let nextMax = max
   let nextMin = 1
   if (d === 1) {
     nextMax = SUM - m[0]
@@ -34,6 +36,7 @@ const magic = (m, d, t) => {
     // [x, ?, ?, -]
     // [?, ?, ?, -]
     t[0] = SUM - (m[0] + m[1] + m[2])
+    if (t[0] <= 0) return
     nextMax = SUM - m[0]
   } else if (d === 4) {
     // [o, o, o, -]
@@ -54,7 +57,7 @@ const magic = (m, d, t) => {
     t[1] = SUM - (m[3] + m[4] + m[5])
     const t3max = SUM - (t[0] + m[5])
     nextMax = SUM - m[0] - m[3]
-    nextMin = SUM - (t3max + m[0] + m[3])
+    nextMin = SUM - t3max + m[0] + m[3]
   } else if (d === 7) {
     // [o, o, o, -]
     // [o, o, o, -]
@@ -72,7 +75,8 @@ const magic = (m, d, t) => {
     // [o, o, o, -]
     // [o, o, o, -]
     // [o, o, x, -]
-    t[4] = SUM - (m[1] + m[4] + m[7])
+    t[4] = SUM - (m[1] + m[4] + m[7]) // 到達するのはおかしい?
+    if (t[4] <= 0) return
     nextMax = SUM - Math.max(m[2] + m[5], m[6] + m[7], m[0] + m[4])
     const t2max = SUM - (t[0] + t[1])
     const t5max = SUM - (t[0] + t[1])
@@ -82,19 +86,38 @@ const magic = (m, d, t) => {
     t[2] = SUM - (m[6] + m[7] + m[8])
     t[5] = SUM - (m[2] + m[5] + m[8])
     t[6] = SUM - (m[0] + m[4] + m[8])
-    p++
     if (
+      t[2] <= 0 ||
+      t[5] <= 0 ||
+      t[6] <= 0 ||
       SUM !== t[0] + t[1] + t[2] + t[6] ||
       SUM !== t[3] + t[4] + t[5] + t[6]
-    ) {
+    )
       return
-    }
 
-    console.log(m.join(','))
-    console.log(t.join(','))
+    const uniq = new Set([...m, ...t]).size >= 15
+    if (!uniq) return
+
+    const l1 = m[0] + m[1] + m[2] + t[0]
+    const l2 = m[3] + m[4] + m[5] + t[1]
+    const l3 = m[6] + m[7] + m[8] + t[2]
+    const l4 = t[3] + t[4] + t[5] + t[6]
+    const c1 = m[0] + m[3] + m[6] + t[3]
+    const c2 = m[1] + m[4] + m[7] + t[4]
+    const c3 = m[2] + m[5] + m[8] + t[5]
+    const x1 = m[0] + m[4] + m[8] + t[6]
+    const x2 = t[0] + m[5] + m[7] + t[3]
+    p++
+    console.log(`
+ ${pad2(m[0])} ${pad2(m[1])} ${pad2(m[2])} ${pad2(t[0])} (${l1}
+ ${pad2(m[3])} ${pad2(m[4])} ${pad2(m[5])} ${pad2(t[1])} (${l2}
+ ${pad2(m[6])} ${pad2(m[7])} ${pad2(m[8])} ${pad2(t[2])} (${l3}
+ ${pad2(t[3])} ${pad2(t[4])} ${pad2(t[5])} ${pad2(t[6])} (${l3}
+(${c1} ${c2} ${c3} ${l4} \\${x1}x/${x2}`)
+
     return
   }
-  for (let j = nextMin; j <= nextMax; j++) {
+  for (let j = Math.max(nextMin, 1); j <= Math.min(nextMax, max); j++) {
     m[d] = j
     magic(m, d + 1, t)
   }
