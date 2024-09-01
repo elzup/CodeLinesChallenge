6d6f2d16984ca2d71c1a59c555dc40995decfff2
elzup <guild0105@gmail.com>
2024-09-01 19:34:41 +0900

    add sound

--- 111days/040-metronom.ts
+++ 111days/040-metronom.ts
@@ -1,4 +1,5 @@
-const bpm = 1000
+import { AudioContext } from 'node-web-audio-api'
+const bpm = 100
 
 const clear = () => {
   process.stdout.write('\x1b[2K')
@@ -10,8 +11,25 @@ const tick = () => {
   clear()
   n++
   process.stdout.write((n % 2 === 0 ? 'x _' : '_ x') + `  ${n} (${bpm}bpm)`)
+  sound()
 }
 
 setInterval(tick, 60000 / bpm)
+const audioContext = new AudioContext()
+
+const sound = () => {
+  // AudioContextを作成
+
+  const oscillator = audioContext.createOscillator()
+  oscillator.type = 'sine' // 波形のタイプ（'sine', 'square', 'sawtooth', 'triangle' など）
+  oscillator.frequency.setValueAtTime(440, audioContext.currentTime) // 周波数を設定（ここでは440Hz、A音）
+
+  oscillator.connect(audioContext.destination)
+  oscillator.start()
+
+  setTimeout(() => {
+    oscillator.stop()
+  }, 100)
+}
 
 export {}
