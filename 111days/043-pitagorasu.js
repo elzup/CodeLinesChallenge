4011a81e3e2c1e86c3efc4e868e815c95f81b97e
elzup <guild0105@gmail.com>
2024-09-05 09:00:48 +0900

    gcd

--- 111days/043-pitagorasu.js
+++ 111days/043-pitagorasu.js
@@ -1,11 +1,25 @@
 // ピタゴラス数を探す
 const hits = {}
 
+function gcd(a, b) {
+  // ユークリッドの互除法で最大公約数を求める
+  while (b !== 0) {
+    let temp = b
+    b = a % b
+    a = temp
+  }
+  return a
+}
+
+function gcd3(a, b, c) {
+  return gcd(a, gcd(b, c)) === 1
+}
+
 for (let a = 1; a <= 300; a++) {
   for (let b = a + 1; b <= 500; b++) {
     const c = Math.sqrt(a ** 2 + b ** 2)
 
-    if (Math.floor(c) === c && b != c) {
+    if (Math.floor(c) === c && b != c && gcd3(a, b, c)) {
       console.log(a, b, c)
       hits[a] = true
       hits[b] = true
