916bedcbaa1aeed1e7cfe3de411a30c64e78dcdd
elzup <guild0105@gmail.com>
2024-09-20 12:21:05 +0900

    day58

--- 111days/058-golden.js
+++ 111days/058-golden.js
@@ -4,12 +4,18 @@ const golden = (1 + Math.sqrt(5)) / 2
 
 const arr = []
 
-for (let i = 1; i < 100; i++) {
-  for (let j = 1; j < 100; j++) {
-    arr.push({ i, j, golden: i / j, diff: golden - i / j })
+let min = { i: 1, j: 1, golden: 1, diff: 1 }
+for (let i = 1; i < 10000; i++) {
+  for (let j = 1; j < 10000; j++) {
+    const rate = i / j
+    const diff = golden - rate
+    if (Math.abs(diff) < Math.abs(min.diff)) {
+      min = { i, j, golden: rate, diff }
+    }
   }
 }
 
-arr.sort((a, b) => Math.abs(a.diff) - Math.abs(b.diff))
+// arr.sort((a, b) => Math.abs(a.diff) - Math.abs(b.diff))
+// console.log(arr.slice(0, 10))
 
-console.log(arr.slice(0, 10))
+console.log(min)
