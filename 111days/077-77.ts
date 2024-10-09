f06e602e307398b9e0b752873a3b9bf80a7e6b40
elzup <guild0105@gmail.com>
2024-10-10 01:11:45 +0900

    update

--- 111days/077-77.ts
+++ 111days/077-77.ts
@@ -4,8 +4,8 @@
 const highlight = (s) => `\u001b[31m${s}\u001b[0m`
 const hl77 = (s) => s.replace(/77/g, highlight('77'))
 
-for (let i = 1; ; i++) {
-  const n = 77n ** BigInt(i)
+for (let i = 3000; ; i++) {
+  const n = 7n ** BigInt(i)
   const count77 = String(n).match(/77/g)?.length
   if (count77 >= 77) {
     console.log(i)
@@ -14,3 +14,8 @@ for (let i = 1; ; i++) {
     break
   }
 }
+
+// const x = 7n ** 7787n
+// console.log(x)
+// console.log(String(x).match(/77/g).length)
+// console.log(hl77(String(x)))
